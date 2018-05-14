48e907296e714bc6b6fc8baefb53231c76d4f419
bangash <liaqatpcr@gmail.com>
2018-05-15 00:58:27 +0500

    (remove css)

---
+++ include/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+ <TITLE>Haidri Blood Bank </TITLE>
+<link rel="stylesheet" href="css/all.css" type="text/css">
+<link rel="stylesheet" href="css/style_main.css" type="text/css">
+<!--<link rel="stylesheet" href="css/default.css" type="text/css">
+<link rel="stylesheet" href="css/report_styles.css" type="text/css">
+<link rel="stylesheet" href="css/template_style.css" type="text/css">-->
+
+</head>
+<body bgcolor="#ffffff" link="#000066" alink="#cc0000" vlink="#000066"  >
+<table width="100%" border=0 cellspacing=0  >
+<tbody class="main">
+	<?PHP include('include/header_menu.php');?>
+</tbody></table>
---
+++ include/header_menu.php
@@ -0,0 +1,27 @@
+<tr>
+		<td  valign="top" align="center" >
+			 <table cellspacing="0"  class="titlebar" border=0>
+ <tr valign=top  class="titlebar" >
+  <td bgcolor="#99ccff" align="left">
+    <font size="4" color="#330066">We Care Hospital Management System</font><br>       </td>
+  </tr>
+ </table>
+ 		</td>
+	</tr>
+
+	<tr>
+		<td  valign="top"  align="center" height="35">
+			
+
+<table width="100%" align="center" valign="bottom" border="0" cellspacing="0" cellpadding="0"><tr align="center"><td  valign="bottom" width="auto" align="center"><DIV id="tabs"><UL>
+	<li ><a href ='#'><SPAN>Dashboard</SPAN></a></li>
+	<li id="current" ><a href ='add_stock.php'><SPAN>Stock</SPAN></a></li>
+	<li ><a href ='#'><SPAN>Phamacy</SPAN></a></li>
+	<li ><a href ='#'><SPAN>Laboritoy</SPAN></a></li>
+	<li ><a href ='#'><SPAN>Clinics</SPAN></a></li>
+	<li ><a href ='#'><SPAN>Reports</SPAN></a></li>
+
+
+</UL></DIV></td></tr><tr><td><div id="doll"></div></td></tr></table>
+		</td>
+	</tr>
